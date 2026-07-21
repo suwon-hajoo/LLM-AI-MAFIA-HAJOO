5e5cbe7a48f0a47815fd3ffd716595f886595f35
ehrl1225 <wjdwntls1225@naver.com>
2026-07-21 12:13:10 +0900

    llm 연동 커밋

---
+++ Assets/Scripts/LLM/OpenAIDTO.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+
+
+[Serializable]
+public class OpenAIRequest
+{
+    public string model;
+    public List<OpenAIMessage> messages;
+    public int max_tokens = 4096;
+    public int temperature = 1;
+    public double top_p = 0.95;
+}
+
+
+[Serializable]
+public class OpenAIMessage
+{
+    public string role;
+    public string content;
+}
+
+[Serializable]
+public class OpenAIResponse
+{
+    public List<OpenAIChoice> choices;
+}
+
+[Serializable]
+public class OpenAIChoice
+{
+    public OpenAIMessage message;
+}
+    
+
