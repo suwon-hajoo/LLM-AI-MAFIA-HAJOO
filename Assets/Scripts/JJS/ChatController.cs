16ae8246b78efeee14bb94a77160683197bef3dd
ehrl1225 <wjdwntls1225@naver.com>
2026-07-22 12:20:42 +0900

    10 llm 질문   답변 채팅 (#15)
    * 채팅 구현
    
    * AI 연결
    
    * 채팅 구현
    
    * AI 연결

---
+++ Assets/Scripts/JJS/ChatController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+public class ChatController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject textChatPrefab;
+    [SerializeField]
+    private Transform parentContent;
+    [SerializeField]
+    private TMP_InputField inputField;
+    private OpenAIChatManager openAIChatManager;
+
+    // 테스트용
+    private GameConversation gameConversation;
+
+    void Start()
+    {
+        openAIChatManager = OpenAIChatManager.Instance;
+        gameConversation = new("한국어로 대답해줘");
+    }
+
+    public void UpdateChat()
+    {
+        if (!inputField.isFocused) return;
+        if (inputField.text.Equals("")) return;
+
+        GameObject clone = Instantiate(textChatPrefab, parentContent);
+        clone.GetComponent<TextMeshProUGUI>().text=  $"[나] : {inputField.text}";
+        MakeAnswer(inputField.text);
+        inputField.text = "";
+    }
+
+    // 이건 테스트용
+    private async void MakeAnswer(string question)
+    {
+        string answer = await openAIChatManager.SendChatRequest(gameConversation, question);
+        gameConversation.Add("user", "user", question);
+        gameConversation.Add("assistant", "assistant", answer);
+        GameObject clone = Instantiate(textChatPrefab, parentContent);
+        clone.GetComponent<TextMeshProUGUI>().text=$"[AI] : {answer}";
+    }
+}
--- Assets/Scripts/LLM/OpenAIChatManager.cs
+++ Assets/Scripts/LLM/OpenAIChatManager.cs
@@ -1,28 +1,41 @@
+#nullable enable
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.Networking;
 
 
-public class OpenAIChatManager: MonoBehaviour
+public class OpenAIChatManager:MonoBehaviour
 {
     private string apiKey = "";
     private string apiUrl = "https://integrate.api.nvidia.com/v1/chat/completions";
+    public static OpenAIChatManager? Instance {get; private set;}
+
+    void Awake()
+    {
+        EnvLoader.Load();
+        apiKey = EnvLoader.Get("Open_AI_API_Key") ?? "";
+        Instance ??= this;
+    }
 
     void Start()
     {
         
     }
 
-    public IEnumerator SendChatRequest(List<OpenAIMessage> message_list, string userPrompt)
+    public async Task<string?> SendChatRequest(GameConversation gameConversation, string userPrompt)
     {
         OpenAIRequest requestData = new()
         {
             model = "google/diffusiongemma-26b-a4b-it",
-            messages = message_list
+            messages = gameConversation.MessageList
         };
 
         string jsonPayload = JsonUtility.ToJson(requestData);
@@ -36,12 +49,12 @@ public class OpenAIChatManager: MonoBehaviour
         request.SetRequestHeader("Content-Type", "application/json");
         request.SetRequestHeader("Authorization", "Bearer " + apiKey);
 
-        yield return request.SendWebRequest();
+        await request.SendWebRequest();
 
         if (request.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError($"API 호출 실패: {request.error}\n상세에러: {request.downloadHandler.text}");
-            yield break;
+            return null;
         }
 
         string jsonResponse = request.downloadHandler.text;
@@ -51,7 +64,9 @@ public class OpenAIChatManager: MonoBehaviour
         {
             string reply = responseData.choices[0].message.content;
             Debug.Log($"<color=green>[LLM 답변]:</color> {reply}");
+            return reply;
         }
+        return null;
     }
 }
     
